376eee1325f1175a4a703c9439276124b1dac47c
FAMILY <FAMILY@DESKTOP-TQU64SV>
2024-02-20 13:58:09 +0900

    Filter 활용의 예시

---
+++ servlet/src/main/java/com/servlet/test/FlowFilterThird.java
@@ -0,0 +1,39 @@
+package com.servlet.test;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(filterName = "timer", urlPatterns = "/third")
+public class FlowFilterThird implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		long startTime = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		long endTime = System.currentTimeMillis();
+		long executeTime = endTime - startTime;
+		System.out.println("수행 시간 :" + executeTime + "ms");
+
+	}
+
+}
