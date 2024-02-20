a3808925f26deb15bf05dd0146fabf48753f8ddf
FAMILY <FAMILY@DESKTOP-TQU64SV>
2024-02-20 10:19:19 +0900

    filter를 적용하는 또 다른 방법. init()에 init-param 지정하기

--- servlet/src/main/java/com/servlet/test/FlowFilterTwo.java
+++ servlet/src/main/java/com/servlet/test/FlowFilterTwo.java
@@ -10,7 +10,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class FlowFilterTwo implements Filter{
-
+	String charset;
+	
 	@Override
 	public void destroy() {
 		System.out.println("destroy()호출 ...... two");
@@ -19,7 +20,8 @@ public class FlowFilterTwo implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		request.setCharacterEncoding("UTF-8");
+		//request.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding(charset);
 		
 		System.out.println("doFilter 호출 전 ...... two");
 		chain.doFilter(request, response);
@@ -29,6 +31,7 @@ public class FlowFilterTwo implements Filter{
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		System.out.println("init() 호출 ...... two");
+		charset = filterConfig.getInitParameter("en");
 	}
 
 }
